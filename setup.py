0b26982ee48b0a855df273e0a6b3c9760fbc6928
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-07-09 13:28:54 +0200

    Imported upstream version '2.0.2' of 'upstream'

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = 'rqt_service_caller'
 
 setup(
     name=package_name,
-    version='1.5.3',
+    version='2.0.2',
     packages=[package_name],
     package_dir={'': 'src'},
     data_files=[
